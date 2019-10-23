d1208a081f6d6ea80b3b48cceba6f1080ee12769
siko408 <gamerbloxhd@gmail.com>
2019-10-23 15:09:22 -0700

    Optimized programs

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -3,32 +3,15 @@ from random import randint
 
 
 def get_words(index):
-    """ In this program I am looping the amount of words desired.
-        I open the file once to get a random index and open it once again to
-        loop over the file once more. I then print the random words into a single line,
-        and placing a period at the end of the final word"""
-
-    index = int(index)
-    words = []
-    indicator = 0
-    for _ in range(index):
-        # Looping over amount desired
-        if index > 0:
-            random_index = randint(0, len(open("/usr/share/dict/words", "r").readlines()) - 1)  # Collecting random index
-        else:
-            random_index = 0
-        for word in open("/usr/share/dict/words", "r").readlines():  # Opening file
-            if indicator == random_index:  # Check if the index matches the indicator
-                words.append(word[0:len(word) - 1])  # If so append the word from the index given
-                break
-            indicator += 1
-        indicator = 0  # Reset indicator to 0 everytime
+    amount = int(index)
+    file = open("words.txt", "r")
+    words = {}
+    for index_2, word in enumerate(file.readlines(), start=1):
+        words.update({index_2: word[0:len(word) - 1]})
+    file.close()
     result = ""
-    for pick in words:
-        if pick == words[len(words) - 1]:
-            result += pick + "."
-        else:
-            result += pick + " "
+    for counter in range(amount):
+        result += words[randint(0, len(words) - 1)] + " "
     print(result)
 
 
--- Code/rearrange.py
+++ Code/rearrange.py
@@ -3,13 +3,21 @@ from random import randint
 """ Simple program that randomizes the words inputted and outputs them in the console.
     Edge cases such as determinating wheather result is an int or not is not added yet"""
 
-if __name__ == "__main__":
-    params = sys.argv[1:]
+
+def update(index, list, value):
+    list = list
+    print("This is the list at first", list)
+    list[index] = value
+    print("This is after updating value:", list)
+    return list
+
+def rearrange(sentence):
     words_list = []
-    for word in params:
+    final_result = ""
+    for word in sentence:
         words_list.append(word)
     print("Reading words in order but randomized")
-    chosen = []
+    chosen = []  # Since i can't define the size of list
     for index in range(len(words_list)):
         pick = randint(0, len(words_list) - 1)
         while True:
@@ -18,12 +26,24 @@ if __name__ == "__main__":
                 if x == pick:
                     result = True
             if result is False:
-                print(words_list[pick], end=" ")
+                # print(words_list[pick], end=" ")
+                final_result += words_list[pick] + " "
                 chosen.append(pick)
+                #update(chosen[index], words_list, words_list[pick])
                 break
             else:
                 pick = randint(0, len(words_list) - 1)
-
+    print(final_result)
+if __name__ == "__main__":
+    params = sys.argv[1:]
+    sentence = "Flying bats and scary cats"
+    word = sentence.split()
+    rearrange(word)
+    print("\nNow we will print these in reverse")
+    #last_index = len()-1
+    #print(last_index)
+    sentence = ""
+    #print("This is the worlds list:", words_list)
     # print("\nReading words in reverse")
     # last_index = len(words_list) - 1
     # while last_index >= 0:

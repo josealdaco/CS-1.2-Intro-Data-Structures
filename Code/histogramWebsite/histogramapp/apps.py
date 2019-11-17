ad09a22309db73d99ab74dcfd0d996b5ec601ed3
siko408 <gamerbloxhd@gmail.com>
2019-11-17 12:17:41 -0800

    django folder added

---
+++ Code/histogramWebsite/histogramapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HistogramappConfig(AppConfig):
+    name = 'histogramapp'
---
+++ Code/histogramWebsite/histogramapp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+# This syntax imports all of the functions and classes
+# inside the views.py in the same folder.
+from . import views
+
+urlpatterns = [
+    url(r'^home/$', views.home_page),
+    url(r'^histogramapp/$', views.main_page),
+    url(r'^profile/$', views.userHistogram.as_view()),
+    url(r'^sentence/$', views.userHistogramSentence.as_view())
+
+]

9701f4c9049770cd365cd5f27751783f8e112484
siko408 <gamerbloxhd@gmail.com>
2019-12-06 19:46:24 -0800

    [Bug Fix]

--- Code/markov_chain.py
+++ Code/markov_chain.py
@@ -18,25 +18,27 @@ def sample(dict):
 def nestedList(value, list):
     """ Finds index of nested list"""
     index = 0
-    for items in list:
-        if items[0] == value:
-            return index
-        index += 1
-    return None
+    try:
+        for items in list:
+            if items[0] == value:
+                return index
+                index += 1
+        return None
+    except TypeError:
+        return None
 
 
-def sample_v2(chain, chainList):
+def sample_v2(chainList):
     """Return a word from this histogram, randomly sampled by weighting
     each word's probability of being chosen by its observed frequency."""
     total = 0
-    dict = chain[0]
     for item in chainList:
-        total += chain[0].get(item)
+        total += item[1]
     result = random.randint(1, total)  # Random value from 1 to totalValues
     for word in chainList:
-        if result - dict[word] <= 0:
+        if result - word[1] <= 0:
             return word
-        result -= dict[word]
+        result -= word[1]
 
 
 def word_filter(word_list):
@@ -45,7 +47,6 @@ def word_filter(word_list):
     if len(word_list) != 0:
         for index, word in enumerate(words):
             new_string = word.translate({ord(i): '' for i in """,?!,":;@$*[.][]#()~``,â€œâ™"""})
-            new_string = new_string.replace('', "")  #  translate({ord(i): ' ' for i in """"""})
             new_string = new_string.strip()
             new_string = ''.join([x for x in new_string if x in string.printable])
             words[index] = new_string.lower()
@@ -66,10 +67,13 @@ def listToString(list):
 def markov_chain_v2(key, chain):
     """ Creating a mokup of the histogram n state """
     chainList = chain[1].get(key)
+    indicator = nestedList('', chainList)  #  This will return index of key or None
+    if indicator is not None:
+        chainList.pop(indicator)
     if chainList is None:
         return "."  # If that word has no next one
     else:
-        return sample_v2(chain, chainList)
+        return sample_v2(chainList)
 
 
 def markov_Dict(size, corpus):
@@ -89,23 +93,21 @@ def markov_Dict(size, corpus):
             secondDictValue = secondDict.get(keyString)
             value += 1
             stateDict.update({keyString: value})
-
-            if secondDictValue is not None:
-                print("addding new keyState", keyState, keyString, "secondDictValue", secondDictValue)
+            indicator = nestedList(listToString(keyState), secondDictValue)
+            if secondDictValue is not None and indicator is None:
                 secondDictValue.append([listToString(keyState), 1])
                 secondDict.update({keyString: secondDictValue})
-                print(secondDict.get(keyString))
             else:
-                if secondDictValue is not None:
+                if secondDictValue is not None and indicator is not None:
                     valueIndex = nestedList(listToString(keyState), secondDictValue)
-                    print("Version 1:", secondDictValue[valueIndex], secondDictValue[valueIndex][1])
+                    #print("Version 1:", secondDictValue[valueIndex], secondDictValue[valueIndex][1])
                     secondDictValue[valueIndex][1] += 1
-                    print("Version 2:", secondDictValue[valueIndex][1])
-                    secondDict.update({listToString(keyState): secondDictValue})
+                    # print("Version 2:", secondDictValue[valueIndex][1])
+                    secondDict.update({keyString: secondDictValue})
         elif keyString not in stateDict and len(keyString.split()) == size:
+
             stateDict.update({keyString: 1})
             if len(keyState) == size:
-                print("this is the keyString:", keyString, listToString(keyState))
                 secondDict.update({keyString: [[listToString(keyState), 1]]})
     result.append(stateDict)
     result.append(secondDict)
@@ -113,18 +115,18 @@ def markov_Dict(size, corpus):
 
 
 def lastIndex(list):
-    return list[len(list) - 1]
+    stringThread = list.split()
+    return stringThread[len(stringThread) - 1]
 
 
 def createSentence(wordAmount, corpus):
-    result = markov_Dict(1, corpus)
+    result = markov_Dict(10, corpus)
     start = sample(result[0])
-    print(result[1])
     print(start + " ", end='')
     for time in range(wordAmount):
         #  previous = [] This will be used later on so the previous statements have not  been said
-        start = markov_chain_v2(start, result)
-        print(lastIndex(start.split()) + " ", end='')
+        start = markov_chain_v2(start, result)[0]
+        print(lastIndex(start) + " ", end='')
         if start == '.':
             start = sample(result[0])
 
@@ -133,4 +135,4 @@ def createSentence(wordAmount, corpus):
 
 
 if __name__ == "__main__":
-    result = createSentence(4, "I am I like I like I know am nothing like  nothing more like like")
+    result = createSentence(4, "I like nothing more then to simply hug you. You have been very good to me and I will never forget that, Really appreciate you.")
